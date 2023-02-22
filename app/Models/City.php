64ef51dffb82111adc1f2a626cdf68f36b5f28a7
sara55mohamed <smohamedamer4@gmail.com>
2023-02-22 12:43:21 +0200

    I finished adding 13 tables with models's

--- app/Models/City.php
+++ app/Models/City.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\CarBooking;    // HasMany
 use App\Models\AqarBooking;    // HasMany
+use App\Models\Area;    // HasMany
 class City extends Model
 {
     use HasFactory,SoftDeletes;
@@ -25,12 +26,15 @@ class City extends Model
     public function carBooking(){
         return $this->HasMany(CarBooking::class);
     }
-
     public function categories(){
         return $this->HasMany(Category::class);
     }
     // relations
     public function aqarBooking(){
         return $this->HasMany(AqarBooking::class);
+    }
+    // relations
+    public function area(){
+        return $this->HasMany(Area::class);
     }
 }
---
+++ database/migrations/2023_02_14_075228_create_areas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name_ar')->required();
+            $table->string('name_en')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->boolean('active')->required()->default(0);
+            $table->foreignId('city_id')->required( )->references('id')->on('cities')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+};
