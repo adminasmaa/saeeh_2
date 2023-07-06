d1c3983212993ac27fc0de1c8feba89129586d9b
AmiraIbrahim25 <alamira.931125@gmail.com>
2023-07-06 09:55:53 +0300

    cities

--- routes/web.php
+++ routes/web.php
@@ -42,11 +42,11 @@ Route::get('registers', 'App\Http\Controllers\Frontend\AuthController@register')
 Route::post('createaccount', 'App\Http\Controllers\Frontend\AuthController@createaccount')->name('createaccount');
 });
 Route::post('addContacts', 'App\Http\Controllers\Frontend\ContactController@addContacts')->name('addContacts');
-define('MAINASSETS', URL::asset('assets'));
-define('FRONTASSETS', URL::asset('frontend/assets'));
+define('MAINASSETS', URL::asset('public/assets'));
+define('FRONTASSETS', URL::asset('public/frontend/assets'));
 define('MAINUPLOADS', URL::asset('uploads'));
 define('MAINDIST', URL::asset('dist/frontend/img'));
-define('MAINDASHBOARD', URL::asset('dashboard_files'));
+define('MAINDASHBOARD', URL::asset('public/dashboard_files'));
 
 
 
