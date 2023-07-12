503d98393ca732afbe973714270077b3fdb11c1b
AmiraIbrahim25 <alamira.931125@gmail.com>
2023-07-12 16:12:08 +0300

    setting

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,8 @@ Route::post('CheckCar', 'App\Http\Controllers\Frontend\CarController@CheckCar')-
 
 });
 Route::post('addContacts', 'App\Http\Controllers\Frontend\ContactController@addContacts')->name('addContacts');
-define('MAINASSETS', URL::asset('public/assets'));
-define('FRONTASSETS', URL::asset('public/frontend/assets'));
+define('MAINASSETS', URL::asset('assets'));
+define('FRONTASSETS', URL::asset('frontend/assets'));
 define('MAINUPLOADS', URL::asset('uploads'));
 define('MAINDIST', URL::asset('dist/frontend/img'));
 define('MAINDASHBOARD', URL::asset('public/dashboard_files'));
