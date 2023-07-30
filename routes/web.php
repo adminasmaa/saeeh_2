cae69260bd7b5be88a5cec8653b85d4df0d2dfef
AmiraIbrahim25 <alamira.931125@gmail.com>
2023-07-30 12:28:09 +0300

    css

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,8 @@ Route::get('checkallcar', 'App\Http\Controllers\Frontend\CarController@CheckCar'
 
 });
 Route::post('addContacts', 'App\Http\Controllers\Frontend\ContactController@addContacts')->name('addContacts');
-define('MAINASSETS', URL::asset('assets'));
-define('FRONTASSETS', URL::asset('frontend/assets'));
+define('MAINASSETS', URL::asset('public/assets'));
+define('FRONTASSETS', URL::asset('public/frontend/assets'));
 define('MAINUPLOADS', URL::asset('uploads'));
 define('MAINDIST', URL::asset('dist/frontend/img'));
 define('MAINDASHBOARD', URL::asset('public/dashboard_files'));
