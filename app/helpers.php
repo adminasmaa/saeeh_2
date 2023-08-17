3bcf07e18cbeb187350c7c6532bb600c6ada3419
sara55mohamed <smohamedamer4@gmail.com>
2023-08-17 09:34:14 +0300

    problem

--- app/helpers.php
+++ app/helpers.php
@@ -5,6 +5,22 @@ use Intervention\Image\Facades\Image;
 function UploadImage($path, $image, $model, $request)
 {
 
+    // $img = $request->file('profile_image');
+    // $image = $request->profile_image->extension();
+    // $imageName = uniqid() . '.' . $image;
+    // $image_resize = Image::make($img->getRealPath());
+    // $image_resize->resize(636,852);        
+    // $path = 'uploads/' . $imageName;
+    // $image_resize->save($path);
+    // $imageUri = 'uploads/' . $imageName; 
+    // $path = storage_path() . '/app/public/uploads/users/' . Hashids::encode($User->id) . '/' . $file_temp_name;
+    // $img = Image::make($file)->fit(1024);
+    // $img->save($path);
+
+    // $image = Image::make($filename);
+    // $image->resize(250,125, function($constraint){
+    //     $constraint->aspectRatio();
+    // })->save($filename);
 
     $thumbnail = $request;
     $destinationPath = $path;
@@ -12,6 +28,7 @@ function UploadImage($path, $image, $model, $request)
     $thumbnail->move($destinationPath, $filename);
     // $thumbnail->resize(1080, 1080);
     // $thumbnail = Image::make(public_path() . '/'.$path.'/' . $filename);
+    // $thumbnail->resize(1080,1080);
     $thumbnail = Image::make(public_path() . '/'.$path.'/' . $filename);
     $thumbnail->insert(public_path('/images/logo.png'), 'bottom-left', -10, -5)->save(public_path($path.'/' . $filename));
     $model->$image = $filename;
