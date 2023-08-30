2e4767f048fe5469137160fd78e3422a083f9caa
asmaa karam abozied <asmaaabozied907@gmail.com>
2023-08-30 13:36:58 +0300

    updated

--- app/Http/Controllers/Frontend/AuthController.php
+++ app/Http/Controllers/Frontend/AuthController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Support\Facades\Auth;
 use Response;
 
 
@@ -10,86 +12,83 @@ use App\Models\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class AuthController extends Controller
 {
 
     public function login()
     {
-       
+
         return view('frontend.login');
 
 
     }
-    
+
     public function register()
     {
-       
+
         return view('frontend.register');
 
 
     }
 
-    public function createaccount( Request $request){
-
-       $request->validate([
-
+    public function createaccount(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
             'email' => 'required|email|string|unique:users',
             'phone' => 'required|string|unique:users',
 
             'password' => 'required|confirmed',
             'name' => 'required',
-        ],
-            [
-                'password.regex' => __('site.password_regex'),
-            ]
-        );
+        ]);
 
+        if ($validation->fails()) {
+            return response()->json(['errors' => $validation->errors()], 422);
+        }
 
-               $request_data = $request->except(['password','password_confirmation','_token']);
+        $request_data = $request->except(['password', 'password_confirmation', '_token']);
 
-             $request_data['active'] = 1;
+        $request_data['active'] = 1;
         $request_data['account_type'] = 'User';
 
         $request_data['password'] = bcrypt($request->password);
         $request_data['username'] = $request->name;
         $request_data['firstname'] = $request->name;
 
-               $user=User::create($request_data);
-               Auth::login;
+        $user = User::create($request_data);
 
+        Auth::login($user);
 
-    return back();
+        return response()->json(['status' => true, 'content' => 'success', 'data' => $user]);
+
     }
 
 
-    public function checklogin(Request $request){
+    public function checklogin(Request $request)
+    {
 
-
-       $request->validate([
-
+        $validation = Validator::make($request->all(), [
             'phone' => 'required',
 
             'password' => 'required',
-        ],
-            [
-            ]
-        );
+        ]);
 
-         if (auth()->attempt(['phone' => $request->phone, 'password' => $request->password])) {
+        if ($validation->fails()) {
+            return response()->json(['errors' => $validation->errors()], 422);
+        }
+        if (auth()->attempt(['phone' => $request->phone, 'password' => $request->password])) {
             $user = Auth::user();
+            Auth::login($user);
+            return response()->json(['status' => true, 'content' => 'success', 'data' => $user]);
 
-            return redirect(route('Home'));
-         }else {
+        } else {
 
 
-         return back()->with('error in crediantal phone or password');
-            }
+            return response()->json(['status' => true, 'content' => 'error']);
+        }
 
     }
-
-
 
 
 }
