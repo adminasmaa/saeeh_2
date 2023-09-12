ee086956995c2cdbab3aab60d358d5d08608fa88
sara55mohamed <smohamedamer4@gmail.com>
2023-09-12 15:06:02 +0300

    push again

--- app/Http/Resources/AqarResource.php
+++ app/Http/Resources/AqarResource.php
@@ -32,22 +32,22 @@ class AqarResource extends JsonResource
             "name" => $this->$name,
             "description" =>preg_replace( "/\r|\n/", "", strip_tags($this->description) ) ?? '',
 
-            "category" => $this->category ?? '',
-            "status" => $this->status ?? '',
+            "category" => new staticResource($this->category),
+            // "status" => $this->status ?? '',
 //            "rate" => $this->aqarComment->avg('rating') ?? '',
             "favorite" => (count(AquarUser::where('aqar_id','=',$this->id)->where('user_id','=',Auth::id())->get())>0 ? true : false),
 //            "rate" => round($this->aqarComment->avg('rating')) ?? 0,
             "rate" => round(AqarReview::where('user_id', '=', Auth::id())->where('aqar_id', '=',$this->id)->avg('rate')) ?? 0,
 
-            "policy_place" =>  html_entity_decode($this->policy_place) ?? '',
+            // "policy_place" =>  html_entity_decode($this->policy_place) ?? '',
 
-            "distance" => $this->distance ?? '',
-            "time" => $this->time ?? '',
+            // "distance" => $this->distance ?? '',
+            // "time" => $this->time ?? '',
             "image" => asset('images/aqars')."/".$this->main_image,
-            "comment_text" => $this->comment_text ?? '',
-            "comision" => $this->comision ?? '',
+            // "comment_text" => $this->comment_text ?? '',
+            // "comision" => $this->comision ?? '',
             "fixed_price" => $this->fixed_price ?? 0,
-            "Reservation_deposit" => $this->fixed_price ?? 0,
+            // "Reservation_deposit" => $this->fixed_price ?? 0,
             "changed_price" => $this->changed_price?(json_decode($this->changed_price)->person_num[0]?json_decode($this->changed_price) : NULL):NULL,
 
 
