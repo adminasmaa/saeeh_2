54950d905adedb65a1e3b9a444bc9b12d36b4d83
sara55mohamed <smohamedamer4@gmail.com>
2023-09-24 10:14:57 +0300

    push

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -25,7 +25,7 @@ class CommentResource extends JsonResource
             "description" => preg_replace("/\r|\n/", "", strip_tags($this->description)) ?? '',
            // "rate" => round(AqarReview::where('user_id', '=', $user_id)->where('aqar_id', '=', $this->aqar_id)->avg('rate')) ?? 0,
 
-            "rate " => $this->rating ?? 0,
+            "rate" => $this->rating ?? 0,
 //            "status" => $this->status ?? '',
             'user' => new UserResource($this->user),
             "created_at" => $this->created_at ?? '',
