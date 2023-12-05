f76f13a0bdb96e49a24723861426fa07148d0e89
asmaa karam abozied <asmaaabozied907@gmail.com>
2023-12-05 14:39:15 +0200

    updated

--- routes/web.php
+++ routes/web.php
@@ -120,11 +120,11 @@ Route::prefix(LaravelLocalization::setLocale())->middleware('localeSessionRedire
 
 
 Route::post('addContacts', 'App\Http\Controllers\Frontend\ContactController@addContacts')->name('addContacts');
-define('MAINASSETS', URL::asset('assets'));
-define('FRONTASSETS', URL::asset('frontend/assets'));
-define('MAINUPLOADS', URL::asset('uploads'));
-define('MAINDIST', URL::asset('dist/frontend/img'));
-define('MAINDASHBOARD', URL::asset('dashboard_files'));
+define('MAINASSETS', URL::asset('public/assets'));
+define('FRONTASSETS', URL::asset('public/frontend/assets'));
+define('MAINUPLOADS', URL::asset('public/uploads'));
+define('MAINDIST', URL::asset('public/dist/frontend/img'));
+define('MAINDASHBOARD', URL::asset('public/dashboard_files'));
 
 if (!defined('constant')) define('constant', 'value');
 
